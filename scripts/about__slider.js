b414e26b6bd3849507171190f1ff04dbeb0d8704
Kashia Troian <kashia.veb@gmail.com>
2023-07-24 18:37:33 +0300

    slider adaptive

--- scripts/about__slider.js
+++ scripts/about__slider.js
@@ -3,5 +3,24 @@ $(document).ready(function(){
         slidesToShow: 3,
         slidesToScroll: 1,
         arrows: false,
+        responsive: [
+            {
+              breakpoint: 960,
+              settings: {
+                slidesToShow: 2,
+                slidesToScroll: 1
+              }
+            },
+            {
+              breakpoint: 720,
+              settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1
+              }
+            }
+            // You can unslick at a given breakpoint now by adding:
+            // settings: "unslick"
+            // instead of a settings object
+          ]
     });
   });
